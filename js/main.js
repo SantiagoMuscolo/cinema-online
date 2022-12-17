180f36fd7b3d48efb11d6e34feea7e5000286298
Santiago Muscolo <smuscolo2003@gmail.com>
2022-12-16 23:34:49 -0300

    agregue html a mi proyecto

--- js/main.js
+++ js/main.js
@@ -1,112 +1,78 @@
 
+const peliculasMenores = [{id: 1, nombre:"DragonBall", precio: 600, categoria: 'menor'}, {id: 2,nombre: "OnePiece", precio: 600, }, {id: 3,nombre: "Bakugan", precio: 600 , categoria: 'menor'}, {id: 4, nombre: "Los Piratas Del Caribe", precio: 600 , categoria: 'menor'}];
+
+const peliculasMayores = [{id: 5, nombre: "Baby Driver", precio: 1000, categoria: 'mayor'}, {id: 6,nombre: "Sangre, Sudor y Gloria", precio: 1000, categoria: 'mayor'}, {id: 7,nombre: "Terrifier", precio: 1000, categoria: 'mayor'}, {id: 8,nombre: "Anabelle", precio: 1000, categoria: 'mayor'}];
+
 const bienvenida = () => {
     let ingresoNombre = prompt("ingresa tu nombre");
 
-    while (ingresoNombre === null || ingresoNombre === ""){
-        alert ("ingrese un nombre valido");
+    while (ingresoNombre === null || ingresoNombre === "") {
+        alert("ingrese un nombre valido");
         ingresoNombre = prompt("ingresa nuevamente un nombre");
     }
 
-    alert (`Hola ${ingresoNombre}, Bienvenido/a a peliculas online`);
+    alert(`Hola ${ingresoNombre}, Bienvenido/a a peliculas online`);
 
     return ingresoNombre;
 }
 
 const edadPelicula = () => {
-    let ingresoEdad = prompt("ingresa tu edad");
-    while(ingresoEdad === ""){
-        ingresoEdad = prompt("ingresa tu edad");
+    let ingresoEdad = parseInt(prompt("ingresa tu edad"));
+    while (isNaN(ingresoEdad)) {
+        ingresoEdad = parseInt(prompt("ingresa tu edad"));
     }
     return ingresoEdad;
 }
 
 const seleccionPelicula = (edad) => {
-    if (edad <= 17){
-        let pelicula = prompt("Estas son nuestras recomendaciones para menores: \n 1-DragonBall \n 2-OnePiece \n 3-Bakugan \n 4-Los Piratas Del Caribe \n \n ingresa el numero de la pelicula que deseas ver");
-        while(pelicula === null || pelicula === "" || pelicula < 0 || pelicula == 0 || pelicula > 4){
-            pelicula = prompt("Estas son nuestras recomendaciones para menores: \n 1-DragonBall \n 2-OnePiece \n 3-Bakugan \n 4-Los Piratas Del Caribe \n \n ingresa el numero de la pelicula que deseas ver");
-        }
-        switch (pelicula) {
-            case "1":
-                alert("ingresaste DragonBall");
-                return "dragon ball";
+    if (edad <= 17) {
+        let pelicula = parseInt(prompt("Estas son nuestras recomendaciones para menores: \n 1-DragonBall \n 2-OnePiece \n 3-Bakugan \n 4-Los Piratas Del Caribe \n \n ingresa el numero de la pelicula que deseas ver"));
+        const filtradoMenores = peliculasMenores.find((el) => {
+            return el.id === pelicula;
+        })
+        alert(`Elegiste la pelicula: ${filtradoMenores.nombre}`);
 
-            case "2":
-                alert("OnePiece");
-                return "one piece";
-
-            case "3":
-                alert("ingresaste Bakugan");
-                return "bakugan";
-
-            case "4":
-                alert("ingresaste Los Piratas Del Caribe");
-                return "los piratas del caribe";
-        }
-    }else if (edad >= 18){
-        let pelicula = prompt("Estas son nuestras recomendaciones para mayores: \n 1-Baby Driver \n 2-Sangre, sudor y gloria \n 3-Terrifier \n 4-Anabelle \n \n ingresa el numero de  la pelicula que deseas ver");
-        while(pelicula === null || pelicula === "" || pelicula < 0 || pelicula == 0 || pelicula > 4){
-            pelicula = prompt("Estas son nuestras recomendaciones para mayores: \n 1-Baby Driver \n 2-Sangre, sudor y gloria \n 3-Terrifier \n 4-Anabelle \n \n ingresa el numero de  la pelicula que deseas ver");
-        }
-        switch (pelicula) {
-            case "1":
-                alert("ingresaste Baby Driver");
-                return "baby driver";
-        
-            case "2":
-                alert("ingresaste Sangre, sudor y gloria");
-                return "sangre, sudor y gloria";
-
-            case "3":
-                alert("ingresaste Terrifier");
-                return "terrifier";
-
-            case "4":
-                alert("ingresaste Anabelle");
-                return "anabelle";
-        }
+    while (isNaN(pelicula)) {
+        pelicula = parseInt(prompt("Estas son nuestras recomendaciones para menores: \n 1-DragonBall \n 2-OnePiece \n 3-Bakugan \n 4-Los Piratas Del Caribe \n \n ingresa el numero de la pelicula que deseas ver"));
     }
+
+    return filtradoMenores;
+    
+}else if (edad >= 18) {
+    let pelicula = parseInt(prompt("Estas son nuestras recomendaciones para mayores: \n 5-Baby Driver \n 6-Sangre, sudor y gloria \n 7-Terrifier \n 8-Anabelle \n \n ingresa el numero de  la pelicula que deseas ver"));
+
+    const filtradoMayores = peliculasMayores.find((el) => {
+        return el.id === pelicula;
+    })
+
+    alert(`Elegiste la pelicula: ${filtradoMayores.nombre}`);
+
+    while (isNaN(pelicula)) {
+        pelicula = parseInt(prompt("Estas son nuestras recomendaciones para mayores: \n 5-Baby Driver \n 6-Sangre, sudor y gloria \n 7-Terrifier \n 8-Anabelle \n \n ingresa el numero de  la pelicula que deseas ver"));
+    }
+
+    return filtradoMayores;
+}
 }
 
-const precioPelicula = (pelicula) => {
-    if(pelicula === "dragon ball"){
-        return 600;
-    }else if (pelicula === "one piece"){
-        return 600;
-    }else if (pelicula === "bakugan"){
-        return 600;
-    }else if (pelicula === "los piratas del caribe"){
-        return 600;
-    }else if (pelicula === "baby driver"){
-        return 1000;
-    }else if (pelicula === "sangre, sudor y gloria"){
-        return 1000;
-    }else if (pelicula === "terrifier"){
-        return 1000;
-    }else if (pelicula === "anabelle"){
-        return 1000;
-    }
-}
-
-const cobro = (nombre, pelicula, precio) => {
-        alert(`la pelicula que eligio es ${pelicula} y el costo de su entrada sera de ${precio} pesos`);
+const cobro = (pelicula) => {
+    alert(`la pelicula que eligio es ${pelicula.nombre} y el costo de su entrada sera de ${pelicula.precio} pesos`);
     let abonar = parseInt(prompt("con cuanto abonara?"));
-    while(isNaN(abonar) || abonar <= 0){
+    while (isNaN(abonar) || abonar <= 0) {
         abonar = parseInt(prompt("con cuanto abonara?"));
     }
-    if(abonar > precio ){
-        let resultado = abonar-precio;
+    if (abonar > pelicula.precio) {
+        let resultado = abonar - pelicula.precio;
         alert(`su vuelto es ${resultado}`);
-    }else if(abonar < precio){
+    } else if (abonar < pelicula.precio) {
         alert("te pedimos disculpas pero tu presupuesto no es suficiente");
         return false;
     }
     alert("gracias por su visita, vuelva prontos.");
-    }   
+}
 
 
 let nombre = bienvenida()
 let edad = edadPelicula()
-let pelicula = seleccionPelicula (edad)
-let precio = precioPelicula(pelicula)
-cobro(nombre, pelicula, precio)
+let pelicula = seleccionPelicula(edad)
+cobro(pelicula)
