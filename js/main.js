5ab20c5a65809b8ac7a01372626f82aa027952c3
Santiago Muscolo <smuscolo2003@gmail.com>
2023-01-03 17:52:03 -0300

    agregue archivos de js y html a mi proyecto

--- js/main.js
+++ js/main.js
@@ -1,78 +1,11 @@
+const img = document.querySelector(`#body`);
 
-const peliculasMenores = [{id: 1, nombre:"DragonBall", precio: 600, categoria: 'menor'}, {id: 2,nombre: "OnePiece", precio: 600, }, {id: 3,nombre: "Bakugan", precio: 600 , categoria: 'menor'}, {id: 4, nombre: "Los Piratas Del Caribe", precio: 600 , categoria: 'menor'}];
+const backgrounds = [`avengers.jpg`, `cyberpunk.jpg`, `spiderman.jpg`];
 
-const peliculasMayores = [{id: 5, nombre: "Baby Driver", precio: 1000, categoria: 'mayor'}, {id: 6,nombre: "Sangre, Sudor y Gloria", precio: 1000, categoria: 'mayor'}, {id: 7,nombre: "Terrifier", precio: 1000, categoria: 'mayor'}, {id: 8,nombre: "Anabelle", precio: 1000, categoria: 'mayor'}];
-
-const bienvenida = () => {
-    let ingresoNombre = prompt("ingresa tu nombre");
-
-    while (ingresoNombre === null || ingresoNombre === "") {
-        alert("ingrese un nombre valido");
-        ingresoNombre = prompt("ingresa nuevamente un nombre");
-    }
-
-    alert(`Hola ${ingresoNombre}, Bienvenido/a a peliculas online`);
-
-    return ingresoNombre;
+function cambiarImagen(fondos){
+    let random = Math.floor(Math.random() * fondos.length)
+    img.setAttribute("src", `../imagenes/backgrounds/${backgrounds[random]}`);
+    console.log(random);
 }
 
-const edadPelicula = () => {
-    let ingresoEdad = parseInt(prompt("ingresa tu edad"));
-    while (isNaN(ingresoEdad)) {
-        ingresoEdad = parseInt(prompt("ingresa tu edad"));
-    }
-    return ingresoEdad;
-}
-
-const seleccionPelicula = (edad) => {
-    if (edad <= 17) {
-        let pelicula = parseInt(prompt("Estas son nuestras recomendaciones para menores: \n 1-DragonBall \n 2-OnePiece \n 3-Bakugan \n 4-Los Piratas Del Caribe \n \n ingresa el numero de la pelicula que deseas ver"));
-        const filtradoMenores = peliculasMenores.find((el) => {
-            return el.id === pelicula;
-        })
-        alert(`Elegiste la pelicula: ${filtradoMenores.nombre}`);
-
-    while (isNaN(pelicula)) {
-        pelicula = parseInt(prompt("Estas son nuestras recomendaciones para menores: \n 1-DragonBall \n 2-OnePiece \n 3-Bakugan \n 4-Los Piratas Del Caribe \n \n ingresa el numero de la pelicula que deseas ver"));
-    }
-
-    return filtradoMenores;
-    
-}else if (edad >= 18) {
-    let pelicula = parseInt(prompt("Estas son nuestras recomendaciones para mayores: \n 5-Baby Driver \n 6-Sangre, sudor y gloria \n 7-Terrifier \n 8-Anabelle \n \n ingresa el numero de  la pelicula que deseas ver"));
-
-    const filtradoMayores = peliculasMayores.find((el) => {
-        return el.id === pelicula;
-    })
-
-    alert(`Elegiste la pelicula: ${filtradoMayores.nombre}`);
-
-    while (isNaN(pelicula)) {
-        pelicula = parseInt(prompt("Estas son nuestras recomendaciones para mayores: \n 5-Baby Driver \n 6-Sangre, sudor y gloria \n 7-Terrifier \n 8-Anabelle \n \n ingresa el numero de  la pelicula que deseas ver"));
-    }
-
-    return filtradoMayores;
-}
-}
-
-const cobro = (pelicula) => {
-    alert(`la pelicula que eligio es ${pelicula.nombre} y el costo de su entrada sera de ${pelicula.precio} pesos`);
-    let abonar = parseInt(prompt("con cuanto abonara?"));
-    while (isNaN(abonar) || abonar <= 0) {
-        abonar = parseInt(prompt("con cuanto abonara?"));
-    }
-    if (abonar > pelicula.precio) {
-        let resultado = abonar - pelicula.precio;
-        alert(`su vuelto es ${resultado}`);
-    } else if (abonar < pelicula.precio) {
-        alert("te pedimos disculpas pero tu presupuesto no es suficiente");
-        return false;
-    }
-    alert("gracias por su visita, vuelva prontos.");
-}
-
-
-let nombre = bienvenida()
-let edad = edadPelicula()
-let pelicula = seleccionPelicula(edad)
-cobro(pelicula)
+window.onload = cambiarImagen(backgrounds)
---
+++ js/register.js
@@ -0,0 +1,50 @@
+const nombre = document.querySelector(`#nombre`),
+usuarioReg = document.querySelector(`#usuario`),
+email = document.querySelector(`#email`),
+passReg = document.querySelector(`#password`),
+btnRegister = document.querySelector(`#button`);
+
+
+let usuarios;
+
+if(localStorage.getItem(`usuarios`)){
+    usuarios = JSON.parse(localStorage.getItem(`usuarios`));
+}else{
+    usuarios = [];
+}
+
+class usuario{
+    constructor(nombre, usuario, email, password){
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.email = email;
+        this.password = password ;
+    }
+}
+
+function limpiarCampos (){
+    nombre.value = "";
+    userReg.value = "";
+    email.value = "";
+    passwordReg.value = "";
+    btnRegister.value = "";
+}
+
+function guardarUsuario(usuario){
+    return usuarios.push(usuario)
+}
+
+function guardarEnElStorage(elemento){
+    return localStorage.setItem(`usuarios`, JSON.stringify(elemento));
+}
+
+btnRegister.addEventListener(`click`, (e=>{
+    e.preventDefault();
+    
+    let nuevoUsuario = new usuario(nombre.value, usuarioReg.value, email.value, passReg.value);
+    console.log(nuevoUsuario);
+    guardarUsuario(nuevoUsuario);
+    guardarEnElStorage(usuarios);
+}))
+
+
