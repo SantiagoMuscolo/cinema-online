29660f4f2e8b854b61a733951ff26034f973b451
Santiago Muscolo <smuscolo2003@gmail.com>
2023-01-10 12:59:58 -0300

    solucione inconvenientes con las cards

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const peliculas = [
     { nombre: "Baby Driver", descripcion: "Para saldar su deuda con un mafioso, un joven con problemas de audición hace de conductor en atracos. Cuando por fin cree que se ha librado de su compromiso y pretende rehacer su vida, se ve obligado a retornar para un último golpe. ", img: "https://pics.filmaffinity.com/Baby_Driver-399368582-large.jpg" },
     { nombre: "Sangre, Sudor y Gloria", descripcion: "Mark Wahlberg y Dwayne Johnson, planean el secuestro de un rico empresario. En el transcurso de la operación creen que han matado a su víctima, pero no es así. el empresario contrata a un detective privado para que localice a sus captores.", img: "https://pics.filmaffinity.com/Sangre_sudor_y_gloria-382155306-large.jpg" },
     { nombre: "Terrifier", descripcion: "El payaso psicópata Art aterroriza a dos chicas durante la noche de Halloween, matando a todos aquellos que se interponen en su camino, es un thriller de horror y suspenso que te dejara sin aliento y palabras mientras mas te adentres en el.", img: "https://pics.filmaffinity.com/Terrifier-643530005-large.jpg" },
-    { nombre: "Anabelle", descripcion: "John Form encuentra el regalo perfecto para Mia, su mujer embarazada: una preciosa muñeca 'vintage' llamada Annabelle. Una noche, una secta satánica les ataca brutalmente. Además, provocan que un ente maligno se apodere de Annabelle buscando este influir terror en sus victimas.", img: "https://pics.filmaffinity.com/Annabelle_Creation-137836637-large.jpg" }
+    { nombre: "Anabelle", descripcion: "John Form encuentra el regalo perfecto para Mia, su mujer embarazada: una preciosa muñeca 'vintage' llamada Annabelle. una secta satánica les ataca brutalmente. provocan que un ente maligno se apodere de Annabelle buscando este influir terror en sus victimas.", img: "https://www.ecartelera.com/carteles/7900/7931/002_m.jpg" }
 ];
 
 const contenedor = document.getElementById(`container`)
@@ -19,8 +19,8 @@ peliculas.forEach((movies) => {
             <img src="${movies.img}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${movies.nombre}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <p class="card-text">${movies.descripcion}</p>
+                    <a href="#" class="btn btn-primary">get your ticket</a>
                 </div>
         </div>`
     contenedor.appendChild(card);
